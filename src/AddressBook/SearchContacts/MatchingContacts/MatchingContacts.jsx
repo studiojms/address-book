f1dafa53988063350d0ad19fab2c873e53afb60f
jefferson.souza <jefferson.souza@synchro.com.br>
2021-03-19 14:51:23 -0300

    fix MatchinContacts render rules

--- src/AddressBook/SearchContacts/MatchingContacts/MatchingContacts.jsx
+++ src/AddressBook/SearchContacts/MatchingContacts/MatchingContacts.jsx
@@ -4,41 +4,36 @@ import classNames from 'classnames';
 import './MatchingContacts.css';
 
 const propTypes = {
-  data: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired,
-  })).isRequired,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired,
+    })
+  ).isRequired,
   highlightedIndex: PropTypes.number,
   downshiftGetMenuProps: PropTypes.func.isRequired,
   downshiftGetItemProps: PropTypes.func.isRequired,
 };
 
-const MatchingContacts = ({
-  data,
-  highlightedIndex,
-  downshiftGetMenuProps,
-  downshiftGetItemProps,
-}) => {
-  // TODO: Something is missing here
-
+const MatchingContacts = ({ data, highlightedIndex, downshiftGetMenuProps, downshiftGetItemProps }) => {
   return (
-    <ul {...downshiftGetMenuProps()} className='matching-contacts'>
-      {data.map((item, index) => (
-        <li
-          {...downshiftGetItemProps({
-            key: item.id,
-            item: item,
-            className: classNames(
-              'matching-contacts__item',
-              /* TODO: Something is wrong here */
-              { 'matching-contacts__item--highlighted': false }
-            ),
-          })}
-        >
-          {item.value}
-        </li>
-      ))}
-    </ul>
+    data.length > 0 && (
+      <ul {...downshiftGetMenuProps()} className="matching-contacts">
+        {data.map((item, index) => (
+          <li
+            {...downshiftGetItemProps({
+              key: item.id,
+              item: item,
+              className: classNames('matching-contacts__item', {
+                'matching-contacts__item--highlighted': index === highlightedIndex,
+              }),
+            })}
+          >
+            {item.value}
+          </li>
+        ))}
+      </ul>
+    )
   );
 };
 
